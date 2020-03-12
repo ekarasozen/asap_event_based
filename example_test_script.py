ff9196fb062ba60d6f28e25328924dac8731f5c3
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-03-11 19:19:01 -0600

    test run script lives here for now

---
+++ example_test_script.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("/Users/ezgikarasozen/Documents/Research/Array_processing/asap_w_obspy_git/")
+from getdata import event
+from getdata import taup
+
+st = event(event_list=['ak014cou8d9l'], network_list=['IM','XM'], station_code="BC", pick="P", channel="SHZ", start_time=20*60, end_time=60*60)
+print(st)
+
+
+
