32ae5263ea5ec105f1d994999cb54f7316f5357d
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-03-12 18:50:41 -0600

    minor edits

--- getdata.py
+++ getdata.py
@@ -31,38 +31,36 @@ def taup(inventory,evlat,evlon,evdep,model_l,phase_list):
     return t
     
 
-def event(event_list, network_list, station_code, pick, channel, start_time, end_time):
-    event_id = ['https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=' + s for s in event_list]
-    for e, lab in enumerate(event_id):
-        time_array = np.empty((0, 100))
-        cat = read_events(event_id[e])
-        evot = cat[0].origins[0].time
-        evlat = cat[0].origins[0].latitude
-        evlon= cat[0].origins[0].longitude
-        evdep= (cat[0].origins[0].depth) / 1000 #convert to km
-        print(event_id[e])
-        if cat[0].origins[0].evaluation_mode == "automatic":
-           print("This event is not yet revised")
-           continue
-        picks = cat[0].picks
-        arrivals = cat[0].origins[0].arrivals
-        inventory = client_wm.get_stations(network=network_list[0], station=station_code+"*")
-        for i in picks:
-            i_station_code = i.waveform_id.station_code
-            i_network_code = i.waveform_id.network_code
-            if (i_network_code==network_list[0] or i_network_code==network_list[1]) and i_station_code[0:2]==station_code:
-                for j in arrivals:
-                    if j.pick_id.id==i.resource_id and j.phase==pick: 
-                        time_array = np.append(time_array, [i.time])
-                        t=i.time
-        if time_array.size == 0:
-           print("There are no available picks from the quakeml file, pick time will be calculated by the Taup")
-           t = evot + taup(inventory,evlat,evlon,evdep,model_l=TauPyModel(model="northak"),phase_list=["P", "Pn", "Pg", "p"])
-        try:
-            st = client_wm.get_waveforms(network_list[0], station_code+"*", "*", channel, (t - start_time), (t + end_time), attach_response=True)
-            if len(st) > 0:
-                print('Waveform data found!')
-        except Exception:
-                print('No waveform data found!')
-                continue
+def event(ev_id, network_list, station_code, pick, channel, start_time, end_time):
+    time_array = np.empty((0, 100))
+    cat = read_events(ev_id)
+    evot = cat[0].origins[0].time
+    evlat = cat[0].origins[0].latitude
+    evlon= cat[0].origins[0].longitude
+    evdep= (cat[0].origins[0].depth) / 1000 #convert to km
+    print(ev_id)
+    if cat[0].origins[0].evaluation_mode == "automatic":
+       print("This event is not yet revised")
+#       continue
+    picks = cat[0].picks
+    arrivals = cat[0].origins[0].arrivals
+    inventory = client_wm.get_stations(network=network_list[0], station=station_code+"*")
+    for i in picks:
+        i_station_code = i.waveform_id.station_code
+        i_network_code = i.waveform_id.network_code
+        if (i_network_code==network_list[0] or i_network_code==network_list[1]) and i_station_code[0:2]==station_code:
+            for j in arrivals:
+                if j.pick_id.id==i.resource_id and j.phase==pick: 
+                    time_array = np.append(time_array, [i.time])
+                    t=i.time
+    if time_array.size == 0:
+       print("There are no available picks from the quakeml file, pick time will be calculated by the Taup")
+       t = evot + taup(inventory,evlat,evlon,evdep,model_l=TauPyModel(model="northak"),phase_list=["P", "Pn", "Pg", "p"])
+    try:
+        st = client_wm.get_waveforms(network_list[0], station_code+"*", "*", channel, (t - start_time), (t + end_time), attach_response=True)
+        if len(st) > 0:
+            print('Waveform data found!')
+    except Exception:
+            print('No waveform data found!')
+#            continue
     return st
