701cce0824712b86b0cfa4fbc8ec3c228074296c
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-03-25 17:45:51 -0600

    overly simplified version of spectral subtraction is added

--- example_test_script.py
+++ example_test_script.py
@@ -4,6 +4,7 @@ from getdata import event
 from getdata import taup
 from prepdata import prep
 from addnoise import whitenoise
+import spectral_subtraction as ss
 import wavelet
 import matplotlib.pyplot as plt
 from obspy import Trace
@@ -52,21 +53,22 @@ for e, lab in enumerate(event_id):
     fig1.savefig(event_list[e] + '.pdf', bbox_inches='tight')
     #DATA WITH NOISE
     nos = len(st)
-    st_wn = st.copy()
+    st_n = st.copy()
     for s in range(nos):
-        st_wn[s].data = whitenoise(st_wn[s],type=2,amplitude=200,min_freq=0.1,max_freq=0.4)
-        #st_wn[s].data = whitenoise(st_wn[s],type=1,amplitude=20)
-    print(st[0].std())
-    scales = wavelet.scales(st_wn[0])
-    t, freq = wavelet.param(st_wn[0],scales)
-    X = wavelet.cwt(st_wn[0],scales)
+        st_n[s].data = whitenoise(st_n[s],type=2,amplitude=200,min_freq=0.1,max_freq=0.4)
+        #st_n[s].data = whitenoise(st_n[s],type=1,amplitude=20)
+    nol = (st_n[0].std())*200
+    print(nol)
+    scales = wavelet.scales(st_n[0])
+    t, freq = wavelet.param(st_n[0],scales)
+    Xn = wavelet.cwt(st_n[0],scales)
     fig2 = plt.figure()
     ax21 = fig2.add_axes([0.1, 0.75, 0.7, 0.2])
     ax22 = fig2.add_axes([0.1, 0.1, 0.7, 0.60], sharex=ax21)
     ax23 = fig2.add_axes([0.83, 0.1, 0.03, 0.6])
     ax24 = fig2.add_axes([0.1, -0.15, 0.7, 0.2], sharex=ax21)
-    ax21.plot(t, st_wn[0].data, 'k')
-    img = ax22.imshow(np.abs(X), extent=[t[0], t[-1], freq[-1], freq[0]],
+    ax21.plot(t, st_n[0].data, 'k')
+    img = ax22.imshow(np.abs(Xn), extent=[t[0], t[-1], freq[-1], freq[0]],
                      aspect='auto', interpolation='nearest', cmap=obspy_sequential)
     # Hackish way to overlay a logarithmic scale over a linearly scaled image.
     ax22.set_ylabel("Frequency [Hz] (log)",labelpad=30)
@@ -76,20 +78,24 @@ for e, lab in enumerate(event_id):
     twin_ax.set_ylim(1, 50)
     ax22.tick_params(which='both', labelleft=False, left=False)
     twin_ax.tick_params(which='both', labelleft=True, left=True, labelright=False)
-    IX = wavelet.icwt(X, st_wn[0])
-    ax24.plot(t, IX, 'k')
-    ax24.set_xlabel("Time after %s [s]" % st_wn[0].stats.starttime)
+    IXn = wavelet.icwt(Xn, st_n[0])
+    ax24.plot(t, IXn, 'k')
+    ax24.set_xlabel("Time after %s [s]" % st_n[0].stats.starttime)
     fig2.colorbar(img, cax=ax23)
     fig2.autofmt_xdate()    
-    fig2.savefig(event_list[e] + '_wn.pdf', bbox_inches='tight')
-    #NOISE REMOVED - THIS PART WILL HAVE ITS OWN MODULE AS "SPECTRAL SUBTRACTION METHODS"
+    fig2.savefig(event_list[e] + '_n.pdf', bbox_inches='tight')
+    #NOISE REMOVAL
     nos = len(st)
-    st_won = st_wn.copy()
+    st_won = st_n.copy()
     scales = wavelet.scales(st_won[0])
     t, freq = wavelet.param(st_won[0],scales)
-    idx = np.where(np.logical_and(freq>=0.1, freq<=0.4))
-    X[idx] = 0
-    X = (X).real
+    N = Xn-X #very very very simplistic
+    Xwon = ss.simple_amplitude(Xn,N)
+    #Xwon = (Xwon).real
+    #idx = np.where(np.logical_and(freq>=0.1, freq<=0.4))
+    #print(X[idx])
+    #Xn[idx] = 0
+    #Xwon = (Xn).real
 #    X = wavelet.cwt(st_won[0],scales)
     fig3 = plt.figure()
     ax31 = fig3.add_axes([0.1, 0.75, 0.7, 0.2])
@@ -97,7 +103,7 @@ for e, lab in enumerate(event_id):
     ax33 = fig3.add_axes([0.83, 0.1, 0.03, 0.6])
     ax34 = fig3.add_axes([0.1, -0.15, 0.7, 0.2], sharex=ax31)
     ax31.plot(t, st_won[0].data, 'k')
-    img = ax32.imshow(np.abs(X), extent=[t[0], t[-1], freq[-1], freq[0]],
+    img = ax32.imshow(np.abs(Xwon), extent=[t[0], t[-1], freq[-1], freq[0]],
                      aspect='auto', interpolation='nearest', cmap=obspy_sequential)
     # Hackish way to overlay a logarithmic scale over a linearly scaled image.
     ax32.set_ylabel("Frequency [Hz] (log)",labelpad=30)
@@ -107,8 +113,8 @@ for e, lab in enumerate(event_id):
     twin_ax.set_ylim(1, 50)
     ax32.tick_params(which='both', labelleft=False, left=False)
     twin_ax.tick_params(which='both', labelleft=True, left=True, labelright=False)
-    IX = wavelet.icwt(X, st_won[0])
-    ax34.plot(t, IX, 'k')
+    IXwon = wavelet.icwt((Xwon), st_won[0]) #Xwon.real? or not? 
+    ax34.plot(t, IXwon, 'k')
     ax34.set_xlabel("Time after %s [s]" % st_won[0].stats.starttime)
     fig3.colorbar(img, cax=ax33)
     fig3.autofmt_xdate()    
