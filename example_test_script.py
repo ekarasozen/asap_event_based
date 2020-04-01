35a77dff3e0ea863d18655e1218d944fce738ead
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-01 12:22:44 -0600

    scaleogram log scale bug is fixed, .json to .py switch is added

--- example_test_script.py
+++ example_test_script.py
@@ -2,8 +2,9 @@ import sys
 sys.path.append("/Users/ezgikarasozen/Documents/Research/Array_processing/asap_w_obspy_git/")
 from getdata import event
 from getdata import taup
-from prepdata import prep
+from getdata import prep
 from addnoise import whitenoise
+import parameters
 import spectral_subtraction as ss
 import wavelet
 import matplotlib.pyplot as plt
@@ -12,17 +13,13 @@ from obspy import Stream
 from obspy.imaging.cm import obspy_sequential
 import numpy as np
 import mlpy.wavelet as wave
-import json
+#import json
 from scipy import signal
-import mlpy.wavelet as wave
 
-file = open('parameters.json')
-inputs = json.loads(file.read())
-event_list = inputs['event_list']
+event_list = parameters.event_list
 
 
-#event_list=['ak0184nb2fzw','ak014cou8d9l']
-#event_list=['ak0184nb2fzw']
+
 event_id = ['https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=' + s for s in event_list]
 for e, lab in enumerate(event_id):
     st = event(ev_id=event_id[e], network_list=['IM','XM'], station_code="BC", pick="P", channel="SHZ", start_time=20, end_time=60)
@@ -40,11 +37,11 @@ for e, lab in enumerate(event_id):
     img = ax12.imshow(np.abs(X), extent=[t[0], t[-1], freq[-1], freq[0]],
                      aspect='auto', interpolation='nearest', cmap=obspy_sequential)
     # Hackish way to overlay a logarithmic scale over a linearly scaled image.
-    ax12.set_ylabel("Frequency [Hz] (log)",labelpad=30)
+    ax12.set_ylabel("Frequency [Hz] (log)")
     twin_ax = ax12.twinx()
     twin_ax.set_yscale('log')
     twin_ax.set_xlim(t[0], t[-1])
-    twin_ax.set_ylim(1, 50)
+    twin_ax.set_ylim(freq[-1], freq[0])
     ax12.tick_params(which='both', labelleft=False, left=False)
     twin_ax.tick_params(which='both', labelleft=True, left=True, labelright=False)
     IX = wavelet.icwt(X, st[0])
@@ -59,8 +56,6 @@ for e, lab in enumerate(event_id):
     for s in range(nos):
         st_n[s].data = whitenoise(st_n[s],type=2,amplitude=200,min_freq=0.1,max_freq=0.4)
         #st_n[s].data = whitenoise(st_n[s],type=1,amplitude=20)
-    nol = (st_n[0].std())*200
-    print(nol)
     scales = wavelet.scales(st_n[0])
     t, freq = wavelet.param(st_n[0],scales)
     Xn = wavelet.cwt(st_n[0],scales)
@@ -77,7 +72,8 @@ for e, lab in enumerate(event_id):
     twin_ax = ax22.twinx()
     twin_ax.set_yscale('log')
     twin_ax.set_xlim(t[0], t[-1])
-    twin_ax.set_ylim(1, 50)
+    twin_ax.set_ylim(freq[-1], freq[0])
+    #twin_ax.set_ylim(1, 50)
     ax22.tick_params(which='both', labelleft=False, left=False)
     twin_ax.tick_params(which='both', labelleft=True, left=True, labelright=False)
     IXn = wavelet.icwt(Xn, st_n[0])
@@ -90,9 +86,11 @@ for e, lab in enumerate(event_id):
     Fs=st_n[0].stats.sampling_rate #sampling rate
     IS=st_n[0].stats.npts
     x=st_n[0].data
-    #n=n[0].data
     hanwin = np.hanning(IS)
     signal.resample(hanwin,int(Fs))
+    idx = np.where(np.logical_and(freq>=0.1, freq<=0.4))
+    print(np.amax(Xn[idx].real))
+    print(np.average(Xn[idx].real))
     n = x*hanwin
     dt_n = 0.05
     dj_n = 0.05
@@ -103,10 +101,8 @@ for e, lab in enumerate(event_id):
     nos = len(st)
     scales = wavelet.scales(st_won[0])
     t, freq = wavelet.param(st_won[0],scales)
-    idx = np.where(np.logical_and(freq>=0.1, freq<=0.4))
-    #N = Xn[]-X #very very very simplistic
     Xwon = Xn
-    Xwon[idx] = ss.simple_amplitude(Xn[idx],N[idx])
+    Xwon[idx] = ss.simple_subtraction(Xn[idx],N[idx],2)
 #    X = wavelet.cwt(st_won[0],scales)
     fig3 = plt.figure()
     ax31 = fig3.add_axes([0.1, 0.75, 0.7, 0.2])
@@ -114,14 +110,14 @@ for e, lab in enumerate(event_id):
     ax33 = fig3.add_axes([0.83, 0.1, 0.03, 0.6])
     ax34 = fig3.add_axes([0.1, -0.15, 0.7, 0.2], sharex=ax31)
     ax31.plot(t, st_won[0].data, 'k')
-    img = ax32.imshow(np.abs(Xwon), extent=[t[0], t[-1], freq[-1], freq[0]],
+    img = ax32.imshow(np.abs(Xwon), extent=[t[0], t[-1], freq[-1], freq[0]], 
                      aspect='auto', interpolation='nearest', cmap=obspy_sequential)
     # Hackish way to overlay a logarithmic scale over a linearly scaled image.
     ax32.set_ylabel("Frequency [Hz] (log)",labelpad=30)
     twin_ax = ax32.twinx()
     twin_ax.set_yscale('log')
     twin_ax.set_xlim(t[0], t[-1])
-    twin_ax.set_ylim(1, 50)
+    twin_ax.set_ylim(freq[-1], freq[0])
     ax32.tick_params(which='both', labelleft=False, left=False)
     twin_ax.tick_params(which='both', labelleft=True, left=True, labelright=False)
     IXwon = wavelet.icwt((Xwon), st_won[0]) #Xwon.real? or not? 
