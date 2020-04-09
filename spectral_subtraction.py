7786ae5706997895c9da15366ef24ce0c25162c8
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-09 15:54:45 -0600

    edited to do the subtraction on amplitude spectrums

--- spectral_subtraction.py
+++ spectral_subtraction.py
@@ -5,18 +5,22 @@ import numpy as np
 def simple_subtraction(Xn, N, p): 
     #if p = 1; amplitude/magnitude subtraction
     #if p = 2; power subtraction
+    aXn = np.abs(Xn)
+    aN = np.abs(N)
     m = (Xn.shape[0])
     n = (Xn.shape[1])
     Xwon = np.zeros((m,n))
     for i in range(0,m):
-        Xwon[i,:] = np.sqrt((Xn[i].real ** p) - (N[i].real ** p))
+        Xwon[i,:] = np.sqrt((aXn[i] ** p) - (aN[i] ** p))
+    np.savetxt('Xwon.out', Xwon, delimiter=',', newline='\n')   # X is an array
     return Xwon 
 
-def over_subtraction(Xn, N): #haven't tested this yet
+def over_subtraction(Xn, N):
     #alpha over subtraction factor, value greater than or equal to 1
     #beta spectral floor parameter value between 0 to 1
-    SNR = (np.average(Xn.real))/(np.std(Xn.real)) #this is for the entire waveform for now
-    print(SNR)
+    aXn = np.abs(Xn)
+    aN = np.abs(N)
+    SNR = (np.average(Xn))/(np.std(Xn)) #this is for the entire waveform for now
     alpha0=4 #vary between 3-6 (Beruiti et al'79), normally taken as 4 (Kamath & Loizou'02)
     beta=0.5 #should be between 0-1.
     if SNR < -5:
@@ -25,12 +29,12 @@ def over_subtraction(Xn, N): #haven't tested this yet
         alpha = alpha0-(3/20*SNR)
     elif SNR >= 20:
         alpha = alpha0-3 
-    m = (Xn.shape[0])
-    n = (Xn.shape[1])
+    m = (aXn.shape[0])
+    n = (aXn.shape[1])
     Xwon = np.zeros((m,n))
     for i in range(0,m):
-        if ((Xn[i,i].real) ** 2) > ((alpha + beta)*((N[i].real) ** 2)):
-            Xwon[i,:] = np.sqrt((Xn[i].real ** 2) - (N[i].real ** 2))
+        if ((aXn[i,i]) ** 2) > ((alpha + beta)*((aN[i]) ** 2)):
+            Xwon[i,:] = np.sqrt((aXn[i] ** 2) - ((alpha)*(aN[i] ** 2)))
         else:
             Xwon = np.sqrt((beta)*(N[i] ** 2))
     return Xwon     
