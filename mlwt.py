9c0526e481680f535fdb37976b3318c57d13af35
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-10 17:15:44 -0600

    line 23, changed scales[1: to [0:

--- mlwt.py
+++ mlwt.py
@@ -6,20 +6,21 @@ from obspy.imaging.cm import obspy_sequential
 # For now, wavelet type, omega0 and dj are set to default
 # type = Morlet, omega0=6, d=0.05
 # Based on Torrence & Compo '97 and obspy example
+#still not sure about renaming st to tr. 
 
 def scales(st):
-    N = len(st.data)
+    L = len(st.data)
     dt = st.stats.delta
     dj = 0.05
     omega0 = 6
-    scales = wave.autoscales(N, dt=dt, dj=dj, wf='morlet', p=omega0)
+    scales = wave.autoscales(L, dt=dt, dj=dj, wf='morlet', p=omega0)
     return scales
 
 def param (st,scales):
     omega0 = 6
     t = np.arange(st.stats.npts) / st.stats.sampling_rate
     #from table 1 in Torrence & Compo '97'
-    freq = (omega0 + np.sqrt(2.0 + omega0 ** 2)) / (4 * np.pi * scales[1:])
+    freq = (omega0 + np.sqrt(2.0 + omega0 ** 2)) / (4 * np.pi * scales[0:])
     return t, freq
 
 def cwt(st,scales):
@@ -32,11 +33,11 @@ def cwt(st,scales):
     return X
 
 def icwt(X, st):
-    N = len(st.data)
+    L = len(st.data)
     dt = st.stats.delta
     dj = 0.05
     omega0 = 6
-    scales = wave.autoscales(N, dt=dt, dj=dj, wf='morlet', p=omega0)
+    scales = wave.autoscales(L, dt=dt, dj=dj, wf='morlet', p=omega0)
     #From Torrence & Compo'97 Table 2
     Cr=0.776 #reconstruction factor 
     Wo=(math.pi)**(-1/4) #Psi0(0)
