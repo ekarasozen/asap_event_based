536de5b156799d6064b78b9f4614e85f6b442a63
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-10 15:15:36 -0600

    renamed Xn to Xd, N to Xn, Xwon to amp_Xp

--- spectral_subtraction.py
+++ spectral_subtraction.py
@@ -2,25 +2,25 @@ import numpy as np
 #Xn: signal with noise
 #N: noise estimate - known for now (D in Fukane and Sahare)
     
-def simple_subtraction(Xn, N, p): 
+def simple_subtraction(Xd, Xn, p): 
     #if p = 1; amplitude/magnitude subtraction
     #if p = 2; power subtraction
-    aXn = np.abs(Xn)
-    aN = np.abs(N)
-    m = (Xn.shape[0])
-    n = (Xn.shape[1])
-    Xwon = np.zeros((m,n))
+    amp_Xd = np.abs(Xd)
+    amp_Xn = np.abs(Xn)
+    m = (Xd.shape[0])
+    n = (Xd.shape[1])
+    amp_Xp = np.zeros((m,n))
     for i in range(0,m):
-        Xwon[i,:] = np.sqrt((aXn[i] ** p) - (aN[i] ** p))
-    np.savetxt('Xwon.out', Xwon, delimiter=',', newline='\n')   # X is an array
-    return Xwon 
+        amp_Xp[i,:] = np.sqrt((amp_Xd[i] ** p) - (amp_Xn[i] ** p))
+#    np.savetxt('Xwon.out', Xwon, delimiter=',', newline='\n')   # X is an array
+    return amp_Xp 
 
-def over_subtraction(Xn, N):
+def over_subtraction(Xd, Xn):
     #alpha over subtraction factor, value greater than or equal to 1
     #beta spectral floor parameter value between 0 to 1
-    aXn = np.abs(Xn)
-    aN = np.abs(N)
-    SNR = (np.average(Xn))/(np.std(Xn)) #this is for the entire waveform for now
+    amp_Xd = np.abs(Xd)
+    amp_Xn = np.abs(Xd)
+    SNR = (np.average(Xd))/(np.std(Xn)) #this is for the entire waveform for now, amp?
     alpha0=4 #vary between 3-6 (Beruiti et al'79), normally taken as 4 (Kamath & Loizou'02)
     beta=0.5 #should be between 0-1.
     if SNR < -5:
@@ -29,12 +29,12 @@ def over_subtraction(Xn, N):
         alpha = alpha0-(3/20*SNR)
     elif SNR >= 20:
         alpha = alpha0-3 
-    m = (aXn.shape[0])
-    n = (aXn.shape[1])
-    Xwon = np.zeros((m,n))
+    m = (amp_Xd.shape[0])
+    n = (amp_Xd.shape[1])
+    amp_Xp = np.zeros((m,n))
     for i in range(0,m):
-        if ((aXn[i,i]) ** 2) > ((alpha + beta)*((aN[i]) ** 2)):
-            Xwon[i,:] = np.sqrt((aXn[i] ** 2) - ((alpha)*(aN[i] ** 2)))
+        if ((amp_Xd[i,i]) ** 2) > ((alpha + beta)*((amp_Xn[i]) ** 2)):
+            amp_Xp[i,:] = np.sqrt((amp_Xd[i] ** 2) - ((alpha)*(amp_Xn[i] ** 2)))
         else:
-            Xwon = np.sqrt((beta)*(N[i] ** 2))
-    return Xwon     
+            amp_Xp[i,:] = np.sqrt((beta)*(amp_Xn[i] ** 2))
+    return amp_Xp     
