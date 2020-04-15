d5ee0b4dd883c462e9c32a063c1e67309fade8f3
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-15 17:32:37 -0600

    cwt paramters are added

--- param.py
+++ param.py
@@ -11,9 +11,16 @@ filter_freqmin = 1
 filter_freqmax = 3
 #whitenoise parameters, trg  [g]arbage (aka, noise) used to purposefully degrade a good signals 
 noise_type = 2 #refer to addnoise for different types of whitenoise that can be added
-noise_amplitude = 200
+noise_amplitude = 50
 noise_freqmin = 1
 noise_freqmax = 4
-#noise estimation parameters, trn
-ibegin = 0
-iend = 200
+#noise estimation parameters
+ibegin = 25
+iend = 35
+#cwt parameters
+#cwt_type = "mlwt"
+cwt_type = "pycwt"
+dj = 0.05 #scale spacing
+omega0 = 6
+s0 = 0.096801331 # smallest scale, required for pycwt (maybe not? check this), mlpy automatically calculates this
+wf = 'morlet' #type of wavelet for mlpy, is there a way to do this for pycwt too? check
