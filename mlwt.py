93f21899e8691111e6fa09b0cb8bec8dc6fae78d
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-15 17:27:07 -0600

    wavelet parameters like dj, omega0, wf are moved to parameters file

--- mlwt.py
+++ mlwt.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("/Users/ezgikarasozen/Documents/Research/Array_processing/asap_w_obspy_git/")
+import param
 import numpy as np
 import mlpy.wavelet as wave
 import math
@@ -8,16 +11,17 @@ from obspy.imaging.cm import obspy_sequential
 # Based on Torrence & Compo '97 and obspy example
 #still not sure about renaming st to tr. 
 
+dj = param.dj
+omega0 = param.omega0
+wf = param.wf
+
 def scales(st):
     L = len(st.data)
     dt = st.stats.delta
-    dj = 0.05
-    omega0 = 6
-    scales = wave.autoscales(L, dt=dt, dj=dj, wf='morlet', p=omega0)
+    scales = wave.autoscales(L, dt=dt, dj=dj, wf=wf, p=omega0)
     return scales
 
 def param (st,scales):
-    omega0 = 6
     t = np.arange(st.stats.npts) / st.stats.sampling_rate
     #from table 1 in Torrence & Compo '97'
     freq = (omega0 + np.sqrt(2.0 + omega0 ** 2)) / (4 * np.pi * scales[0:])
@@ -25,23 +29,19 @@ def param (st,scales):
 
 def cwt(st,scales):
     dt = st.stats.delta
-    dj = 0.05
     signal = st.data
-    omega0 = 6
     # approximate scales through frequencies
-    X = wave.cwt(x=signal, dt=dt, scales=scales, wf='morlet', p=omega0)
+    X = wave.cwt(x=signal, dt=dt, scales=scales, wf=wf, p=omega0)
     return X
 
 def icwt(X, st):
     L = len(st.data)
     dt = st.stats.delta
-    dj = 0.05
-    omega0 = 6
-    scales = wave.autoscales(L, dt=dt, dj=dj, wf='morlet', p=omega0)
+    scales = wave.autoscales(L, dt=dt, dj=dj, wf=wf, p=omega0)
     #From Torrence & Compo'97 Table 2
     Cr=0.776 #reconstruction factor 
     Wo=(math.pi)**(-1/4) #Psi0(0)
     #missing part of the eqn 11 is defined here as Sc=Scale
     Sc=(dj*math.sqrt(dt))/(Cr*Wo)
-    IX = wave.icwt(X=(X*Sc), dt=dt, scales=scales, wf='morlet', p=omega0)
+    IX = wave.icwt(X=(X*Sc), dt=dt, scales=scales, wf=wf, p=omega0)
     return IX
