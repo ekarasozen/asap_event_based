41a59cc4734cda020bb7616bee73ed325a1b2f0b
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-15 17:34:12 -0600

    minor edits

--- param.py
+++ param.py
@@ -1,4 +1,4 @@
-#waveform parameters #
+###################waveform parameters########################
 event_list=['ak0184nb2fzw','ak014cou8d9l']
 network_list = ['IM','XM']
 station_code = "BC"
@@ -9,15 +9,15 @@ end_time = 60
 filter_type = "bandpass"
 filter_freqmin = 1
 filter_freqmax = 3
-#whitenoise parameters, trg  [g]arbage (aka, noise) used to purposefully degrade a good signals 
+###################noise parameters added to purposefully degrade signals##################
 noise_type = 2 #refer to addnoise for different types of whitenoise that can be added
 noise_amplitude = 50
 noise_freqmin = 1
 noise_freqmax = 4
-#noise estimation parameters
+##################noise estimation parameters##################
 ibegin = 25
 iend = 35
-#cwt parameters
+########################cwt parameters########################
 #cwt_type = "mlwt"
 cwt_type = "pycwt"
 dj = 0.05 #scale spacing
