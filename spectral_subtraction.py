5a0ea01acc318f095de6f4d6250927f1506c704b
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-17 12:57:43 -0600

    testing over subtraction

--- spectral_subtraction.py
+++ spectral_subtraction.py
@@ -6,41 +6,45 @@ def simple_subtraction(amp_Xd, amp_Xn, p):
     #if p = 1; amplitude/magnitude subtraction
     #if p = 2; power subtraction
     #amp_Xd = np.abs(Xd)
-    minamp = 500                # I picked this value randomly
+    minamp = 0                # eqn 10 in Hassani 2011
     m, n = amp_Xd.shape 
     amp_Xp = np.zeros((m,n))
     amp_Xna = np.mean(amp_Xn,axis=1)
     for i in range(0,m):
         amp_Xp[i,:] = (amp_Xd[i,:] ** p) - (amp_Xna[i] ** p)
     result = np.where(amp_Xp<minamp**p)  # find low or negative values
-    amp_Xp[result] = minamp**p           # set minimum value
+    amp_Xp[result] = 0         # set minimum value, eqn 10 Hassani 2011
     amp_Xp = amp_Xp ** (1/p)             # square root has to come AFTER the negatives are removed
+    #amp_Xp = (np.abs(amp_Xp)) ** (1/p)             # eqn 11 in Hassani 2011
     return amp_Xp 
 
-def over_subtraction(amp_Xd, amp_Xn): #still working on this
+def over_subtraction(amp_Xd, amp_Xn, p): #still working on this
     #alpha over subtraction factor, value greater than or equal to 1
     #beta spectral floor parameter value between 0 to 1
-    minamp = 500                # I picked this value randomly
     m, n = amp_Xd.shape 
+    SNR = np.zeros((m))
+    alpha = np.zeros((m))
     amp_Xp = np.zeros((m,n))
+    amp_Xda = np.mean(amp_Xd,axis=1)
     amp_Xna = np.mean(amp_Xn,axis=1)
-    SNR = (np.average(amp_Xd))/(np.std(amp_Xd)) #this is for the entire waveform for now, amp?
-    print(SNR)
     alpha0=4 #vary between 3-6 (Beruiti et al'79), normally taken as 4 (Kamath & Loizou'02)
     beta=0.5 #should be between 0-1.
-    if SNR < -5:
-        alpha = alpha0+(3/4)
-    elif SNR >= -5 and SNR < 20:
-        alpha = alpha0-(3/20*SNR)
-    elif SNR >= 20:
-        alpha = alpha0-3 
-    print(alpha)
     for i in range(0,m):
-        if ((amp_Xd[i,i]) ** 2) > ((alpha + beta)*((amp_Xna[i]) ** 2)):
-            amp_Xp[i,:] = (amp_Xd[i,:] ** 2) - ((alpha)*(amp_Xna[i] ** 2))
-        else:
-            amp_Xp[i,:] = (beta)*(amp_Xna[i] ** 2)
-    result = np.where(amp_Xp<minamp**2)  # find low or negative values
-    amp_Xp[result] = minamp**2           # set minimum value
-    amp_Xp = amp_Xp ** (1/2)             # square root has to come AFTER the negatives are removed
+        SNR[i] = (amp_Xda[i])/(amp_Xna[i])
+        if SNR[i] < -5:
+            alpha[i] = alpha0+(3/4)
+        elif SNR[i] >= -5 and SNR[i] < 20:
+            alpha[i] = alpha0-(3/20*SNR[i])
+        elif SNR[i] >= 20:
+            alpha[i] = alpha0-3 
+        print(alpha)
+        amp_Xp[i,:] = (amp_Xd[i,:] ** p) - ((alpha[i])*(amp_Xna[i] ** p))
+        for j in range(0,n):
+            if amp_Xp[i,j] > (beta)*(amp_Xna[i] ** p): #Hassani 2011
+#            if amp_Xp[i,j] > (beta+alpha[i])*(amp_Xna[i] ** p): Fukane 2011
+                amp_Xp[i,j] = amp_Xp[i,j]
+            else:
+                amp_Xp[i,j] = (beta)*(amp_Xna[i] ** p)
+        #np.savetxt('amp_Xp.out', amp_Xp, delimiter=',', newline="\n")   # X is an array
+    amp_Xp = amp_Xp ** (1/p)             # square root has to come AFTER the negatives are removed
     return amp_Xp     
