8ec3b4220bdecce460f30d4f7be9ff9a35c12d1a
Ezgi Karasozen <ezgikarasozen@gmail.com>
2020-04-27 17:44:43 -0600

    testing SNR

--- example_test_script.py
+++ example_test_script.py
@@ -62,7 +62,7 @@ for e, lab in enumerate(event_id):
         amp_Xn = abs(Xn)
         #amp_Xp = ss.simple_subtraction(amp_Xd,amp_Xn,2)
         #amp_Xp = ss.over_subtraction(amp_Xd,amp_Xn,2)
-        amp_Xp = ss.nonlin_subtraction(amp_Xd,amp_Xn)
+        amp_Xp = ss.nonlin_subtraction(amp_Xd,amp_Xn,2)
         #amp_Xp = ss.mulban_subtraction(amp_Xd,amp_Xn,trd,freq_d)
         phase_Xd = np.angle(Xd)
         Xp = amp_Xp*(np.exp(1.j*phase_Xd))
