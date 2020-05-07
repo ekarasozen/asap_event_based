fec052d00e0db1fbff5b162a43fd1069419f511c
Michael West <mewest@alaska.edu>
2020-05-07 09:17:52 -0800

    added module mikes_helpers.py. See module for description.

---
+++ mikes_helpers.py
@@ -0,0 +1,34 @@
+'''
+This module is a working space for devoloping half-baked functions. Once functions are 
+robust and useful they probably belong somewhere else. Documentation within this module 
+is weak. Generally, you probably want to avoid anything in this module unless you 
+are Mike!
+'''
+
+
+import numpy as np
+from obspy import Stream
+from obspy.signal.cross_correlation import correlate
+from obspy.signal.cross_correlation import xcorr_max
+from matplotlib import pyplot
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+'''
+This module once contained:
+    spectral_subtraction.simple_subtraction
+    myplot.subtraction_performance
+    measure.waveform_metrics
+'''
+
+
+
+
+    
+    
+    
+    
+    
+    
+
