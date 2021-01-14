67e163c9f93869a0d0c933d5f268c28547771d34
Ezgi Karasozen <ezgikarasozen@gmail.com>
2021-01-14 15:06:19 -0700

    noise type 3 is added

--- addnoise.py
+++ addnoise.py
@@ -1,24 +1,31 @@
 import numpy as np
-def whitenoise(st,t,type,amplitude,min_freq=1,max_freq=3):
+def whitenoise(tro,trd,t,type,amplitude,min_freq=1,max_freq=3):
     #np.random.seed(42)
-    st_temp = st.copy() #[o]riginal signal
-    st_temp.trim(t - 1, t + 5)
-    nl=amplitude*st_temp.std()
-    print(st.std())
-    print(st_temp.std())
-    noe=st.stats.npts 
-    nos=st.stats.sampling_rate 
-    n = st.data.size 
-    timestep = 1/(nos) 
+    trg = tro.copy() #garbage signal
+    tro_temp = tro.copy() 
+    tro_temp.trim(t - 1, t + 5)
+    #print(tro.std())
+    #print(tro_temp.std())
     if type==1: #white noise, all stations
+        noe=tro.stats.npts 
         wn = np.random.normal(0,nl,noe) 
-        st.data = st.data + wn 
+        trd.data = trd.data + wn 
     if type==2: #band-limited white noise
-        f = np.fft.fft(st.data) 
+        nl=amplitude*tro.std()
+        nos=tro.stats.sampling_rate 
+        n = tro.data.size 
+        timestep = 1/(nos) 
+        f = np.fft.fft(trd.data) 
         freqs = np.fft.fftfreq(n, d=timestep) 
         idx = np.where(np.logical_and(freqs>=min_freq, freqs<=max_freq))[0]
-        wn = np.random.normal(0,nl,len(idx))
-        f[idx] += wn
+        wn = np.random.normal(0,1,len(idx))
+        f[idx] += wn*nl
         finv = np.fft.ifft(f).real
-        st.data = finv
-    return st.data
+        trd.data = finv
+    if type==3: #band-limited noise, Mike's version
+        trg.data = np.random.normal(0 ,1 , len(trg.data))
+        #trg0 = trg.copy()               # copy of unfiltered garbage (for plots)
+        trg.filter('bandpass', freqmin=min_freq, freqmax=max_freq, corners=5, zerophase=True)
+#        trg1 = trg.copy()               # copy of filtered garbage (for plots)
+        trd.data = trd.data + (trg.data*amplitude*tro_temp.std())  # ---- degraded data -----
+    return trd.data
